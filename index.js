1b2872efd410d5c5b44ad8dd72284cfddf045c9d
GautamEnergy <157204115+GautamEnergy@users.noreply.github.com>
2024-01-23 16:21:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors())
 async function fetchNews(req,res) {
  const {search,from,Page} = req.query
   try {
-    const {data} = await axios.get(`https://newsapi.org/v2/everything?q=${search}&from=${from}&sortBy=publishedAt&page=${Page}&language=en&pageSize=10&apiKey=b8099662d85f4fbea3b114e739356814`)
+    const {data} = await axios.get(`https://newsapi.org/v2/everything?q=${search}&from=${from}&sortBy=publishedAt&page=${Page}&language=en&pageSize=10&apiKey=b80e023cf58d4838a381be9697e7611a`)
        res.send(data)
      
   } catch (error) {
