1e33a6797e97880f32fe90b732a062227e523fa0
Saifullah Ansari <saifa5098@gmail.com>
2025-02-12 15:50:18 +0530

    Blog Change

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ function extractWebsiteName(domain) {
 }
 const path=require("path")
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+
 app.use('/admin/blogImage', express.static(path.join(__dirname, 'Blog_Images')));
 app.use('/admin/blogVideo', express.static(path.join(__dirname, 'Blog_Video')));
 
