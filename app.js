d1a7b61f2d142399faa6cea198e3f768abd2a47f
Saifullah Ansari <saifa5098@gmail.com>
2025-03-04 12:24:19 +0530

    For galo

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const url = require("url");
+const cookieParser=require('cookie-parser');
 const app = express();
 app.use(express.json());
 app.use(cors());
 require("dotenv").config();
 app.use(bodyParser.json());
+app.use(cookieParser());
+
 
 function extractWebsiteName(domain) {
   const domainParts = domain.split(".");
@@ -20,20 +23,36 @@ function extractWebsiteName(domain) {
   return domainParts.join(".");
 }
 const path = require("path");
+const { GaloRouter } = require("./Routes/galo.routes");
+
+app.use((req, res, next) => {
+ 
+  let reqUrl=req.query.utm_source||null;
+  if(reqUrl!==null){
+     res.cookie('utm_source',reqUrl,{
+      httpOnly:true,
+      secure:true,
+      sameSite:'lax',
+      maxAge:2*60*1000
+     })
+  }
+  next();
+});
+
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
-app.use(
-  "/admin/blogImage",
-  express.static(path.join(__dirname, "Blog_Images"))
-);
+app.use("/admin/blogImage",express.static(path.join(__dirname, "Blog_Images")));
 app.use("/admin/blogVideo", express.static(path.join(__dirname, "Blog_Video")));
+
+app.use('/galo_admin/blogImage',express.static(path.join(__dirname,'Galo_Blog_Images')))
+app.use('/galo_admin/blogVideo',express.static(path.join(__dirname,'Galo_Blog_Video')))
 
 // Nodemailer configuration for SMTP
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "gautamsolar.vidoes01@gmail.com", 
+    user: "gautamsolar.vidoes01@gmail.com",
     pass: "cxmxypwbaupolgqo",
   },
   tls: {
@@ -44,7 +63,7 @@ const transporter = nodemailer.createTransport({
 const transporter1 = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "galoenergy55@gmail.com", 
+    user: "galoenergy55@gmail.com",
     pass: "weqskxtfzscaddar",
   },
   tls: {
@@ -66,8 +85,6 @@ app.post("/submit-contactus", async (req, res) => {
     const checkboxList = checkboxValues
       .map((key) => `<li>${key}: Yes</li>`)
       .join("");
-    const utmSource =
-      req.body.utm_source || req.query.utm_source || "Not provided";
 
     // Construct the email content
     const mailOptions = {
@@ -87,7 +104,7 @@ app.post("/submit-contactus", async (req, res) => {
         ${checkboxList}
       </ul>
       <p style="margin-bottom: 10px;"><strong>Source:</strong> ${referrerWebsite}</p>
-      <p style="margin-bottom: 10px;"><strong>UTM Source:</strong> ${utmSource}</p>
+      <p style="margin-bottom: 10px;"><strong>UTM Source:</strong> ${req.cookies.utm_source?req.cookies.utm_source:'Not Provided'}</p>
     </div>
       `,
     };
@@ -109,8 +126,7 @@ app.post("/submit-contactbox", async (req, res) => {
     const referrerUrl = req.headers.referer || "Unknown"; // Get the referrer URL
     const referrerDomain = url.parse(referrerUrl).hostname; // Extract the domain name from the URL
     const referrerWebsite = extractWebsiteName(referrerDomain); // Extract the website name from the domain name
-    const utmSource =
-      req.body.utm_source || req.query.utm_source || "Not provided";
+    // const utmSource = req.body.utm_source || req.query.utm_source || "Not provided";
 
     // ... (Any additional validation or processing for Contact Box form data)
 
@@ -128,7 +144,7 @@ app.post("/submit-contactbox", async (req, res) => {
       <p style="margin-bottom: 10px;"><strong>Willing to invest:</strong> ${formData.option}</p>
       <p style="margin-bottom: 10px;"><strong>Remarks:</strong> ${formData.message}</p>
       <p style="margin-bottom: 10px;"><strong>Source:</strong> ${referrerWebsite}</p>
-        <p style="margin-bottom: 10px;"><strong>UTM Source:</strong> ${utmSource}</p>
+        <p style="margin-bottom: 10px;"><strong>UTM Source:</strong> ${req.cookies.utm_source?req.cookies.utm_source:'Not Provided'}</p>
     </div>
       `,
     };
@@ -150,8 +166,7 @@ app.post("/submit-solarplant", async (req, res) => {
     const referrerUrl = req.headers.referer || "Unknown"; // Get the referrer URL
     const referrerDomain = url.parse(referrerUrl).hostname; // Extract the domain name from the URL
     const referrerWebsite = extractWebsiteName(referrerDomain); // Extract the website name from the domain name
-    const utmSource =
-      req.body.utm_source || req.query.utm_source || "Not provided";
+    // const utmSource =req.body.utm_source || req.query.utm_source || "Not provided";
 
     // ... (Any additional validation or processing for Contact Box form data)
 
@@ -169,7 +184,7 @@ app.post("/submit-solarplant", async (req, res) => {
       <p style="margin-bottom: 10px;"><strong>Wants to put up:</strong> ${formData.option}</p>
       <p style="margin-bottom: 10px;"><strong>Project Size:</strong> ${formData.sizeoption}</p>
       <p style="margin-bottom: 10px;"><strong>Source:</strong> ${referrerWebsite}</p>
-        <p style="margin-bottom: 10px;"><strong>UTM Source:</strong> ${utmSource}</p>
+        <p style="margin-bottom: 10px;"><strong>UTM Source:</strong> ${req.cookies.utm_source?req.cookies.utm_source:'Not Provided'}</p>
     </div>
       `,
     };
@@ -192,8 +207,7 @@ app.post("/submit-pmkusum", async (req, res) => {
     const referrerWebsite = extractWebsiteName(referrerDomain); // Extract the website name from the domain name
 
     // ... (Any additional validation or processing for Contact Box form data)
-    const utmSource =
-      req.body.utm_source || req.query.utm_source || "Not provided";
+    // const utmSource = req.body.utm_source || req.query.utm_source || "Not provided";
 
     const mailOptions = {
       from: "gautamsolar.vidoes01@gmail.com", // sender email
@@ -208,7 +222,7 @@ app.post("/submit-pmkusum", async (req, res) => {
       <p style="margin-bottom: 10px;"><strong>Wants to put up:</strong> ${formData.projectSize}</p>
       <p style="margin-bottom: 10px;"><strong>Project Size:</strong> ${formData.selectedOption}</p>
       <p style="margin-bottom: 10px;"><strong>Source:</strong> ${referrerWebsite}</p>
-        <p style="margin-bottom: 10px;"><strong>UTM Source:</strong> ${utmSource}</p>
+        <p style="margin-bottom: 10px;"><strong>UTM Source:</strong> ${req.cookies.utm_source?req.cookies.utm_source:'Not Provided'}</p>
     </div>
       `,
     };
@@ -231,8 +245,7 @@ app.post("/submit-delhi", async (req, res) => {
     const referrerWebsite = extractWebsiteName(referrerDomain); // Extract the website name from the domain name
 
     // ... (Any additional validation or processing for Contact Box form data)
-    const utmSource =
-      req.body.utm_source || req.query.utm_source || "Not provided";
+    // const utmSource =req.body.utm_source || req.query.utm_source || "Not provided";
 
     const mailOptions = {
       from: "galoenergy55@gmail.com", // sender email
@@ -251,7 +264,7 @@ app.post("/submit-delhi", async (req, res) => {
       <p style="margin-bottom: 10px;"><strong>Remark:</strong> ${formData.Remark}</p>
      
       <p style="margin-bottom: 10px;"><strong>Source:</strong> ${referrerWebsite}</p>
-        <p style="margin-bottom: 10px;"><strong>UTM Source:</strong> ${utmSource}</p>
+        <p style="margin-bottom: 10px;"><strong>UTM Source:</strong> ${req.cookies.utm_source?req.cookies.utm_source:'Not Provided'}</p>
     </div>
       `,
     };
@@ -268,10 +281,14 @@ app.post("/submit-delhi", async (req, res) => {
 });
 
 app.get("/", (req, res) => {
+  console.log(req.cookies.utm_source?req.cookies.utm_source:'Not provided');
   res.send({ msg: "Welcome Solar News App" });
 });
 
 app.use("/admin", UserRouter);
+
+app.use('/galo_admin',GaloRouter);
+
 
 app.listen(process.env.PORT, async () => {
   try {
